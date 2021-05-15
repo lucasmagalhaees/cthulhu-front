d7ce0c5ff72918060af2139dc36d4473f774a760
Lucas Barbosa <lucasbarbosa1738@gmail.com>
2021-05-15 17:11:09 -0300

    develop: Criando primeiro component react

---
+++ src/components/TestComponent/TestComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './TestComponent.css'
+
+const TestComponent = () => <div className="TestComponent">
+  Test Component!!!! Testando 123! Is it working?
+</div>
+
+export default TestComponent
