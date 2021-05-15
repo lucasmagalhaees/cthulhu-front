650556f2cf76c0237db5af18ecdc475ffbff1717
Lucas Barbosa <lucasbarbosa1738@gmail.com>
2021-05-15 19:53:13 -0300

    develop: Limpeza e organização de arquivos

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,27 +1,7 @@
-import React from "react";
-import logo from "../../logo.svg";
 import "./App.css";
-import TestComponent from "../TestComponent";
-import ClassComponent from "../ClassComponent";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TestComponent name="Lucas"></TestComponent>
-        <ClassComponent name="Mundo"></ClassComponent>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">App</div>;
 }
 
 export default App;
