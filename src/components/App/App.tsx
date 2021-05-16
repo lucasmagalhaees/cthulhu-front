69e824c74650e0d5cb2c7d61d444e83829026e0b
Lucas Barbosa <lucasbarbosa1738@gmail.com>
2021-05-16 15:10:59 -0300

    develop: Criando componente de botão

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,30 @@
+import Button from "../Button";
 import Header from "../Header";
 import "./App.css";
+
+const TestComponent = () => {
+  return (
+    <img
+      src="https://media.istockphoto.com/vectors/magnifying-glass-icon-vector-id986618996"
+      alt="search icon"
+      width="20px"
+      height="20px"
+    />
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <Header title="Algastock"></Header>
-      App testando
+      <div className="Container">
+        <Button
+          onClick={() => {
+            window.alert("event captured");
+          }}
+          appendIcon={<TestComponent />}
+        ></Button>
+      </div>
     </div>
   );
 }
